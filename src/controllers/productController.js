37fc70cce39af473c85f40b69c41a32c30161bbb
Luis <xtremeluigui@hotmail.com>
2022-03-18 23:10:51 -0600

    Branch to analyze the put method

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,9 +1,17 @@
-
 const path = require ('path')
 const fs = require ('fs')
 const productFilePath =path.join (__dirname, '../data/productDataBase.json');
 const product = JSON.parse(fs.readFileSync(productFilePath, 'utf-8'))
-
+let productProto = {
+     id: 16,
+     name: "Sony Srs-xb12 Parlante Bluetooth Portátil",
+     price: 4699,
+     discount: 5,
+     color: "#000000",
+     category: "in-sale",
+     description: "Deja que la música dance cobre vida con EXTRA BASS™ Anima el ambiente con EXTRA BASS™1. Un radiador pasivo trabaja con el parlante monoaural para potenciar los tonos bajos y mejorar los graves, a pesar del tamaño compacto. ",
+     image: "img-parlante-sony.jpg"
+    }
 const productController = {
 
 product: (req, res) => {
@@ -14,10 +22,17 @@ product: (req, res) => {
 },
 editProducts: (req, res) => {
     let id = req.params.id;
-    res.render("editProducts", {id});
+    let prod = product.forEach((item) => {
+        if (item.id == id) {
+            res.render("editProducts", {product : item});
+        }
+    });
+
+    
 },
 createProducts: (req, res) => {
-    res.render("createProducts");
+    let loquesea = req.body;
+    res.render("products", {loquesea : loquesea});
 },
 deleteProducts: (req, res) => {
     let id = req.params.id;
@@ -25,14 +40,38 @@ deleteProducts: (req, res) => {
 
 },
 postProducts: (req, res) => {
-    res.render("home");
+    productProto.id = product[product.length].id + 1;
+    productProto.name = req.body.name;
+    productProto.description = req.body.description;
+    productProto.category = req.body.category;
+    productProto.color = req.body.color;
+    productProto.image = req.file.filename;
+    productProto.price = parseInt(req.body.price);
+
+    product.push(productProto)
+
+    fs.writeFileSync(productFilePath, JSON.stringify(product), 'utf-8')
+    console.log(productProto);
+    console.log(req.file);
+    res.redirect("/");
 },
 putProducts: (req, res) => {
-    res.render("home");
+    let id = req.body.id;
+    console.log("Hola mundo")
+    console.log(req.body)
+    /* let prod = product[id];
+    product[id].name = req.body.name;
+    product[id].description = req.body.description;
+    product[id].color = req.body.color;
+    product[id].image = req.file.filename;
+    product[id].price = parseInt(req.body.price);
+    fs.writeFileSync(productFilePath, JSON.stringify(product), 'utf-8')
+    res.redirect("/"); */
 },
 products: (req, res) => {
     res.render("products",{listaDeProducto: product });
 }
 }
+
 
 module.exports = productController;
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,15 +1,31 @@
+const multer = require('multer');
+const path = require ('path')
 const { Route } = require('express');
 var express = require('express');
 var router = express.Router();
 const productController = require('../controllers/productController');
+const { dirname } = require('path');
+
+const storage = multer.diskStorage({ 
+    destination: function (req, file, cb) { 
+        console.log(__dirname);
+       cb(null, path.join(__dirname, '../../public/images'));
+    }, 
+    filename: function (req, file, cb) { 
+       cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);  } 
+})
+
+const uploadFile = multer({storage});
+
 
 router.get('/:id', productController.product);
 router.get('/', productController.products)
 
+router.get ('/create', productController.createProducts);
 router.get('/:id/edit', productController.editProducts);
-router.get ('/create', productController.createProducts)
+
 router.get ('/delete', productController.deleteProducts)
-router.post ('/', productController.postProducts)
-router.put ('/', productController.putProducts)
+router.post ('/', uploadFile.single('image'),  productController.postProducts)
+router.put('/', productController.putProducts)
 
 module.exports = router;
